2c313fe9c580d3bed05c668dc3d63acbe2426c54
tominkkalarickal <tominkkalarickal@gmail.com>
2025-11-29 11:55:20 -0500

    Add files via upload

---
+++ Tomin Kurian Kalarickal IA1.py
@@ -0,0 +1,35 @@
+#problem_1
+# Input - Prompt for inputs
+height = float(input("Enter height (inches): "))
+weight = float(input("Enter weight (pounds): "))
+
+# Processing - Calculate the BMI result:
+bmi = weight / (height ** 2) * 703
+#convert to metric
+weight_kg = weight * 0.453592
+height_m = height* 0.0254
+#BMI in metric
+bmi_metric = weight_kg / (height_m ** 2)
+
+# Output - Display the result
+print("BMI =", bmi)
+#formatting the output with f string
+print(f'BMI value for a person height of (height) and of weight(weight) formatted using the f string is:(BMI)')
+print(f'BMI value for a person height of (height_m) and of weight(weight_kg) formatted using the f string is:(BMI_METRIC)')
+
+
+# using the round function to round the BMI value to 2 decimal
+print("BMI =", round(bmi,1))
+print("BMI metric =", round(bmi_metric,1))
+
+#problem_2
+#input
+duration_hrs = float(input("Enter the total duration in hours: "))
+speed = float(input("Enter the speed in miles per hour: "))
+#processing
+distance = speed * duration_hrs
+distance= min(distance,30)
+#display the output
+print("Distance =", distance)
+
+
